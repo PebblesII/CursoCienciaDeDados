a0f41c6f09ab8ef80aa94323f60007b20bd3167a
PebblesII <shobell70@gmail.com>
2024-09-24 08:35:32 -0300

    a

---
+++ import math.py
@@ -0,0 +1,14 @@
+import math
+#from pandas import DataFrame
+
+raiz = math.sqrt(9)
+
+print(raiz)
+print(math.ceil(6.1))
+
+
+dados = {"nomes" : ["Golf", "Galingha"], "idade" : ["25", "26"]}
+#dadosFormatatos = DataFrame(dados)
+
+#print(dadosFormatatos)
+
